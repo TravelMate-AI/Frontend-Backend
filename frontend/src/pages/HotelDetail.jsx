45bad2bb8470b8225de8b326d0d9843daf5ecae5
Rasyad Ekardi <152518862+rasyadpe@users.noreply.github.com>
2025-06-12 23:57:51 +0700

    Update HotelDetail.jsx

--- frontend/src/pages/HotelDetail.jsx
+++ frontend/src/pages/HotelDetail.jsx
@@ -1,4 +1,4 @@
-// client/src/pages/RestaurantDetail.jsx
+
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Detail from '../components/Detail'; // Sesuaikan path
@@ -38,8 +38,9 @@ function HotelDetail() {
   if (!hotel) return <div className="text-center py-8 text-gray-500">No hotel data available.</div>;
 
   return (
-    <div className="container mx-auto p-4">
-      <button onClick={() => navigate(-1)} className="mb-4 px-4 py-2 bg-gray-200 rounded-md hover:bg-gray-300">
+    <div className="container mx-auto px-20 py-10">
+      {/* Tombol kembali ke daftar hotel */}
+      <button onClick={() => navigate(-1)} className="mb-4 text-blue-600 hover:underline">
         &larr; Back to Hotels
       </button>
       <Detail
